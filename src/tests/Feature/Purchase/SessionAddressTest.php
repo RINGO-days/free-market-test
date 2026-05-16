b49695a250d37c55f16efd3eb17af8cce69da5b6
RINGO_days <Apple043@outlook.jp>
2026-05-16 21:30:17 +0900

    不要use文削除

--- src/tests/Feature/Purchase/SessionAddressTest.php
+++ src/tests/Feature/Purchase/SessionAddressTest.php
@@ -3,7 +3,6 @@
 namespace Tests\Feature\Purchase;
 
 use Illuminate\Foundation\Testing\RefreshDatabase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use App\Models\User;
 use App\Models\Product;

2e73f92a8213b1ab6e09eb107deb83a5f15c4c65
RINGO-days <Apple043@outlook.jp>
2026-05-19 07:17:58 +0900

    住所検索ボタンのコメントアウトを変更

--- src/resources/views/auth/profile.blade.php
+++ src/resources/views/auth/profile.blade.php
@@ -36,9 +36,9 @@
             <p class="profile-form__ttl">郵便番号</p>
             <div class="post-code__box__inner">
                 <input class="profile-form__input" id="input-post_code" type="text" name="post_code" value="{{old('post_code',$user->post_code)}}">
-                <!-- <button class="address-search_button" formaction="/addressSearch">住所を検索</button> -->
+                <button class="address-search_button" formaction="/addressSearch">住所を検索</button>
                 <!--JavaScriptを用いた住所検索、有効にする場合は上記のボタンタグをコメントアウトする-->
-                <button class="address-search_button" id="button" type="button">住所を検索</button>
+                <!-- <button class="address-search_button" id="button" type="button">住所を検索</button> -->
             </div>
             <div class="error-box">
                 @error('post_code')
